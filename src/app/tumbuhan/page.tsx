e68b0efc9260279afc67a7e38cb7b60fb9b855ff
omarhaqqi24 <muhammadomarhaqqi@gmail.com>
2025-11-16 22:40:45 +0700

    nambahin model tumbuhan

---
+++ src/app/tumbuhan/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import PlantCell from "../../../components/PlantCell";
+
+const HewanPage = () => {
+  return (
+    <div>
+      <PlantCell />
+    </div>
+  );
+};
+
+export default HewanPage;
